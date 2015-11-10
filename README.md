18618e02f3f65821ba67648ae1290c7fada2af2f
Tobias Schifftner <ts@ambimax.de>
2015-11-10 14:33:12 +0100

    Command order:setstatus:complete added

---
+++ README.md
@@ -0,0 +1,44 @@
+MageRun Addons
+==============
+
+Some additional commands for N98-MageRun Magento command-line tool.
+
+
+Installation
+------------
+There are a few options.  You can check out the different options in the [MageRun
+docs](http://magerun.net/introducting-the-new-n98-magerun-module-system/).
+
+Here's the easiest:
+
+1. Create ~/.n98-magerun/modules/ if it doesn't already exist.
+
+        mkdir -p ~/.n98-magerun/modules/
+
+2. Clone the magerun-addons repository in there
+
+        cd ~/.n98-magerun/modules/ && git clone git@github.com:tschifftner/magerun-addons.git
+
+3. It should be installed. To see that it was installed, check to see if one of the new commands is in there, like `order:setstatus:complete`.
+
+        n98-magerun.phar help order:setstatus:complete
+
+Commands
+--------
+
+### Set order complete status ###
+
+This commands creates missing invoices and shipments and sets order status to complete.
+
+    $ n98-magerun.phar order:setstatus:complete [--increment-ids[="100004200-100004250,100003175"]] [--confirm-all]
+
+Every order has to be confirmed manually if ```--config-all``` is not set.
+
+To limit the orders you can specify single, comma separated and ranges of increment ids. You can even combine those like ```--increment-ids=100004215,100004219,100004227-100004238```
+
+
+Credits due where credits due
+--------
+
+ - [magerun](https://github.com/netz98/n98-magerun/)
+ - This readme is based on the readme of [Kalen Jordan addon](https://github.com/kalenjordan/magerun-addons/)
---
+++ src/TSchifftner/Order/Setstatus/CompleteCommand.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace TSchifftner\Order\Setstatus;
+
+use Mage;
+use Mage_Sales_Model_Order;
+use N98\Magento\Command\AbstractMagentoCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CompleteCommand extends AbstractMagentoCommand
+{
+    const ORDER_STATUS = 'complete';
+
+    protected function configure()
+    {
+        $this
+          ->setName('order:setstatus:complete')
+          ->addOption('increment-ids', null, InputOption::VALUE_OPTIONAL, 'Increment ids to select. Comma separated list with range (-) support. For example: 100004200-100004250,100003175,100000000-100000100,100000597')
+          ->addOption('confirm-all', null, InputOption::VALUE_NONE, 'Confirm all by default')
+          ->setDescription('Set complete status of orders')
+        ;
+    }
+
+   /**
+    * @param \Symfony\Component\Console\Input\InputInterface $input
+    * @param \Symfony\Component\Console\Output\OutputInterface $output
+    * @return int|void
+    */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getApplication()->initMagento();
+
+        /** @var Mage_Sales_Model_Resource_Order_Collection $orderCollection */
+        $orderCollection = Mage::getResourceModel('sales/order_collection')
+            ->addAttributeToFilter('status', array('nin' => array(self::ORDER_STATUS)));
+
+
+        // filter order collection by increment ids
+        if($input->getOption('increment-ids')) {
+            $filter = array();
+            $cliIncrementIds = explode(',', $input->getOption('increment-ids'));
+            foreach( (array) $cliIncrementIds as $incrementId) {
+                if(false !== strpos($incrementId, '-')) {
+                    list($from, $to) = explode('-', $incrementId);
+                    $filter[] = array('from' => $from, 'to' => $to);
+                } else {
+                    $filter[] = array('eq' => $incrementId);
+                }
+            }
+            $orderCollection->addAttributeToFilter('increment_id', $filter);
+        }
+
+        // confirm action
+        $orderCount = $orderCollection->count();
+
+        if( ! $orderCount) {
+            $output->writeln('<info>No orders found</info>');
+            return;
+        }
+
+        // inform customer about status
+        $output->writeln(sprintf("<info>There are %s orders found.</info>", $orderCollection->count()));
+
+        // confirm all if not single confirmation requested
+        $question = sprintf(
+            "<question>Are you sure to set completion status for all %s orders without asking?</question> [<comment>n</comment>]",
+            $orderCount
+        );
+
+        $dialog = $this->getHelper('dialog');
+        $singleConfirmation = ! $input->getOption('confirm-all');
+
+        if ( ! $singleConfirmation && ! $dialog->askConfirmation($output, $question, false)) {
+            return;
+        }
+
+        // start applying status
+        foreach($orderCollection as $order) {
+            //$order = $this->_initOrder($incrementId);
+
+            $question = sprintf("<question>Are you sure to set completion status for <comment>[%s]</comment>?</question> [<comment>n</comment>]", $order->getIncrementId());
+            if($singleConfirmation && ! $dialog->askConfirmation($output, $question, false)) {
+                continue;
+            }
+
+            $output->writeln(sprintf("<info>Order %s:</info>", $order->getIncrementId()));
+
+            // create invoice
+            $this->_createInvoice($order, $output);
+
+            // create shipment
+            $this->_createShipment($order, $output);
+
+            $order->save();
+            $output->writeln(sprintf("<info>Status set for %s.</info>", $order->getIncrementId()));
+            $output->writeln('');
+        }
+    }
+
+    /**
+     * Create invoice to order
+     *
+     * @param Mage_Sales_Model_Order $order
+     * @param OutputInterface $output
+     */
+    protected function _createInvoice(Mage_Sales_Model_Order $order, OutputInterface $output)
+    {
+        if($order->hasInvoices()) {
+            $output->writeln('<info>Invoice already exists.</info>');
+            return;
+        }
+
+        if( ! $order->canInvoice()) {
+            $output->writeln('<error>Cannot create invoice.</error>');
+            return;
+        }
+
+        /** @var \Mage_Sales_Model_Service_Order $serviceOrder */
+        $serviceOrder = Mage::getModel('sales/service_order', $order);
+
+        /** @var \Mage_Sales_Model_Order_Invoice $invoice */
+        $invoice = $serviceOrder->prepareInvoice();
+
+        if (!$invoice->getTotalQty()) {
+            $output->writeln('<error>Cannot create an invoice without products.</error>');
+            return;
+        }
+
+        $invoice->setRequestedCaptureCase(\Mage_Sales_Model_Order_Invoice::CAPTURE_OFFLINE);
+        $invoice->register();
+
+        $output->writeln('<info>Invoice created</info>');
+    }
+
+    /**
+     * Create shipment to order
+     *
+     * @param Mage_Sales_Model_Order $order
+     * @param OutputInterface $output
+     */
+    protected function _createShipment(Mage_Sales_Model_Order $order, OutputInterface $output)
+    {
+        if($order->hasShipments()) {
+            $output->writeln('<info>Shipment already exists.</info>');
+            return;
+        }
+
+        if( ! $order->canShip()) {
+            $output->writeln('<error>Cannot create shipment.</error>');
+            return;
+        }
+
+        /** @var \Mage_Sales_Model_Service_Order $serviceOrder */
+        $serviceOrder = Mage::getModel('sales/service_order', $order);
+
+        /** @var \Mage_Sales_Model_Order_Shipment $shipment */
+        $shipment = $serviceOrder->prepareShipment();
+
+        if (!$shipment->getTotalQty()) {
+            $output->writeln('<error>Cannot create a shipment without products.</error>');
+            return;
+        }
+
+        $shipment->register();
+        $output->writeln('<info>Shipment created</info>');
+    }
+}
